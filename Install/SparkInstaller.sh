48e91b52c026cf3c82d4aec4c0d3eaba64a1fe28
Howard <aswergbh888@gmail.com>
2017-02-15 10:53:29 +0800

    Update SparkInstaller.sh

--- Install/SparkInstaller.sh
+++ Install/SparkInstaller.sh
@@ -17,8 +17,8 @@ wget http://d3kbcqa49mib13.cloudfront.net/spark-2.1.0-bin-hadoop2.7.tgz
 tar -xzvf spark-2.1.0-bin-hadoop2.7.tgz
 
 # Environment variable
-echo "export SPARK_HOME=~/spark-2.1.0-bin-hadoop2.7" >> ~/.bashrc
-echo "export SCALA_HOME=~/scala-2.12.1" >> ~/.bashrc
-echo "export JAVA_HOME=/usr/lib/jvm/java-8-oracle" >> ~/.bashrc
-echo "export PATH=$SCALA_HOME/bin:$SPARK_HOME/bin:$JAVA_HOME/bin:$PATH" >> ~/.bashrc
+echo 'export SPARK_HOME=~/spark-2.1.0-bin-hadoop2.7' >> ~/.bashrc
+echo 'export SCALA_HOME=~/scala-2.12.1' >> ~/.bashrc
+echo 'export JAVA_HOME=/usr/lib/jvm/java-8-oracle' >> ~/.bashrc
+echo 'export PATH=$SCALA_HOME/bin:$SPARK_HOME/bin:$JAVA_HOME/bin:$PATH' >> ~/.bashrc
 source ~/.bashrc
